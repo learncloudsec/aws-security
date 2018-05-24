d011943fa774139b5a7122f73b2972a41166291c
csa4-learncloud <csa4@learncloudsec.net>
2018-05-24 15:23:32 +0100

    put updated Ubuntu AMI ID in variables.tf & commented out other regions

--- variables.tf
+++ variables.tf
@@ -10,13 +10,14 @@ variable "aws_region" {
 
 variable "environment" {}
 
-# Ubuntu Precise 12.04 LTS (x64)
+# Ubuntu Xenial 16.04 LTS amd64 
 variable "aws_amis" {
   default = {
-    eu-west-1 = "ami-b1cf19c6"
-    us-east-1 = "ami-de7ab6b6"
-    us-west-1 = "ami-3f75767a"
-    us-west-2 = "ami-21f78e11"
+  
+    eu-west-1 = "ami-35e4db4c"
+    #us-east-1 = "ami-de7ab6b6"
+    #us-west-1 = "ami-3f75767a"
+    #us-west-2 = "ami-21f78e11"
   }
 }
 
