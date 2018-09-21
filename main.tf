73221fae1990c4b9a2d3954e41aee77aae598fa8
csa4-learncloud <csa4@learncloudsec.net>
2018-09-21 11:15:08 +0100

    Restricted SSH to single IP Address

--- main.tf
+++ main.tf
@@ -75,7 +75,7 @@ resource "aws_security_group" "aws-security" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["213.105.202.68/32"]
   }
 
   # HTTP access from the VPC
