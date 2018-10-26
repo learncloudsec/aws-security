1b3132dcc9af616e46cec1da718e316b81dce795
celidor-csa5 <csa5@celidor.net>
2018-10-26 12:00:27 +0100

    restricted SSH to single IP address

--- main.tf
+++ main.tf
@@ -75,7 +75,7 @@ resource "aws_security_group" "aws-security" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["146.176.154.172/32"]
   }
 
   # HTTP access from the VPC
